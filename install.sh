39675d0b8d38ebe3b6f08a86550f0cb557176fbf
mzd245 <seyedimanseyedarab2003@gmail.com>
2020-03-20 04:26:06 +0330

    Initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# adding vazir font to fonts directory 
+if [ ! -d /usr/share/fonts/vazir ];then
+    sudo cp -r fonts/vazir /usr/share/fonts
+fi 
+
+# build font information cache files
+fc-cache -fv
+
+# adding custom font config to telegram tdata 
+CUSTOM_FONT=~/.local/share/TelegramDesktop/tdata 
+if [ ! -f $CUSTOM_FONT ];then 
+    cp fc-custom-1.conf $CUSTOM_FONT
+fi
