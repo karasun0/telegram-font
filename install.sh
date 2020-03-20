73defd06c5822dc64827c19b7840c2eaeb71a410
mzd245 <seyedimanseyedarab2003@gmail.com>
2020-03-21 02:15:42 +0430

    installer: new comments added, some variables used for decrease mistakes

--- install.sh
+++ install.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
-# adding vazir font to fonts directory 
+#Variables 
+TDATA_DIR=~/.local/share/TelegramDesktop/tdata
+TELEGRAM_DESKTOP_FILE=$(ls /usr/share/applications | grep -i telegram)
+EXEC_VAR=$(grep ^Exec /usr/share/applications/telegramdesktop.desktop | cut -d'=' -f2,3)
+
+
+# check telegram '.desktop' file exist or not
+if [ -z $TDATA_DIR ];then
+    echo "Install your Telegram via package manager!(don't use snap or something like that)"
+    exit -1
+fi
+
+
+# adding shabnam font to fonts directory 
 if [ ! -d ~/.local/share/fonts/shabnam ];then
     sudo cp -r fonts/shabnam ~/.local/share/fonts/shabnam
 fi 
 
+
 # build font information cache files
-fc-cache -fv
+fc-cache -fv | grep -i shabnam
+cp fc-custom-1.conf $TDATA_DIR
 
-# adding custom font config to telegram tdata 
-CUSTOM_FONT=~/.local/share/TelegramDesktop/tdata 
-cp fonts.conf $CUSTOM_FONT
+# print Exec variable from '.desktop' file (Usage: debugging stuff)
+echo "Exec value => $EXEC_VAR"
 
-exec_var=$(grep ^Exec /usr/share/applications/telegramdesktop.desktop | cut -d'=' -f2,3)
-echo $exec_var
-sudo sed -ri "s|^Exec=.*|Exec=env FONTCONFIG_FILE=~/.local/share/TelegramDesktop/fonts.conf $exec_var|" /usr/share/applications/telegramdesktop.desktop
+# passing fc-custom-1.conf file to telegram environ
+sudo sed -ri "s|^Exec=.*|Exec=env FONTCONFIG_FILE=$TDATA_DIR/fc-custom-1.conf $EXEC_VAR|" $TELEGRAM_DESKTOP_FILE
+
+EXEC_VAR=$(grep ^Exec $TELEGRAM_DESKTOP_FILE | cut -d'=' -f2,3)
+ 
+# print Exec variable from '.desktop' file (Usage: debugging stuff)
+echo "new Exec vaule => Exec=$EXEC_VAR"
+
+echo "If you run telegram via terminal just add this to your shell config file (bashrc/zshrc/... ):
+    env FONTCONFIG_FILE=$TDATA_DIR/fc-custom-1.conf $EXEC_VAR"
